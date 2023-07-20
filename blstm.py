c502c36eb1da96bf018017df5e7b7d5cddaa896b
geniusaman <108857118+geniusaman@users.noreply.github.com>
2023-07-20 15:42:35 +0530

    Update blstm_new.py

--- blstm.py
+++ blstm.py
@@ -1,50 +1,31 @@
-from __future__ import print_function
-
-import warnings
-
-from keras.utils import to_categorical
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-from sklearn.utils import compute_class_weight
-
-warnings.filterwarnings("ignore")
-
-import numpy as np
-
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM, Bidirectional, LeakyReLU
-from keras.optimizers import Adamax
-
 from sklearn.model_selection import train_test_split
+import numpy as np
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Bidirectional, LSTM, Dense, LeakyReLU, Dropout
+from keras.optimizers import Adamax
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.utils import class_weight
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import confusion_matrix
 
-"""
-Bidirectional LSTM neural network
-Structure consists of two hidden layers and a BLSTM layer
-Parameters, as from the VulDeePecker paper:
-    Nodes: 300
-    Dropout: 0.5
-    Optimizer: Adamax
-    Batch size: 64
-    Epochs: 4
-"""
+
 class BLSTM:
     def __init__(self, data, name="", batch_size=64):
-        vectors = np.stack(data.iloc[:, 1].values)
-        labels = data.iloc[:, 0].values
-        positive_idxs = np.where(labels == 1)[0]
-        negative_idxs = np.where(labels == 0)[0]
-        undersampled_negative_idxs = np.random.choice(negative_idxs, len(positive_idxs), replace=False)
-        resampled_idxs = np.concatenate([positive_idxs, undersampled_negative_idxs])
+        vectors = np.stack(data["vector"].values)
+        labels = data["val"].values
 
-        X_train, X_test, y_train, y_test = train_test_split(vectors[resampled_idxs, ], labels[resampled_idxs],
-                                                            test_size=0.2, stratify=labels[resampled_idxs])
+        # Perform a stratified train-test split
+        X_train, X_test, y_train, y_test = train_test_split(vectors, labels, test_size=0.2, stratify=labels)
+
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = to_categorical(y_train)
         self.y_test = to_categorical(y_test)
         self.name = name
         self.batch_size = batch_size
-        self.class_weight = compute_class_weight(class_weight='balanced', classes=[0, 1], y=labels)
+        self.class_weight = compute_class_weight(class_weight='balanced', classes=np.unique(labels), y=labels)
+
         model = Sequential()
         model.add(Bidirectional(LSTM(300), input_shape=(vectors.shape[1], vectors.shape[2])))
         model.add(Dense(300))
@@ -54,22 +35,24 @@ class BLSTM:
         model.add(LeakyReLU())
         model.add(Dropout(0.5))
         model.add(Dense(2, activation='softmax'))
+
         # Lower learning rate to prevent divergence
         adamax = Adamax(lr=0.002)
         model.compile(adamax, 'categorical_crossentropy', metrics=['accuracy'])
         self.model = model
 
-    """
-    Trains model based on training data
-    """
-    def train(self):
-        self.model.fit(self.X_train, self.y_train, batch_size=self.batch_size, epochs=4, class_weight=self.class_weight)
-        self.model.save_weights(self.name + "_model.h5")
 
-    """
-    Tests accuracy of model based on test data
-    Loads weights from file if no weights are attached to model object
-    """
+    
+    def train(self):
+      # Compute class weights for imbalanced classes
+      y_integers = np.argmax(self.y_train, axis=1)
+      class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(y_integers), y=y_integers)
+      class_weight_dict = dict(enumerate(class_weights))
+
+      self.model.fit(self.X_train, self.y_train, batch_size=self.batch_size, epochs=4, class_weight=class_weight_dict)
+      self.model.save_weights(self.name + "_model.h5")
+
+
     def test(self):
         self.model.load_weights(self.name + "_model.h5")
         values = self.model.evaluate(self.X_test, self.y_test, batch_size=self.batch_size)
